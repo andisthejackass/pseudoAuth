0ff7ea9441ccc8ddf5ff2afea3bf1081d29e437e
Andis Anastasis <andisthejackass@gmail.com>
2012-05-25 01:38:42 +0300

    address print fix

--- server.rb
+++ server.rb
@@ -10,14 +10,13 @@ puts User.first.username
 puts "Starting up server..."
 server = TCPServer.new(1337)
 while (session = server.accept)
- Thread.start do
-   puts "log: Connection from #{session.peeraddr[2]} at
-          #{session.peeraddr[3]}"
-   puts "log: got input from client"
-   input = session.gets
-   puts input
-   session.puts "Server: Welcome #{session.peeraddr[2]}\n"
-   puts "log: sending goodbye"
-   session.puts "Server: Goodbye\n"
- end
+  Thread.start do
+    puts "log: Connection from #{session.peeraddr[2]} at #{session.peeraddr[3]}"
+    puts "log: got input from client"
+    input = session.gets
+    puts input
+    session.puts "Server: Welcome #{session.peeraddr[2]}\n"
+    puts "log: sending goodbye"
+    session.puts "Server: Goodbye\n"
+  end
 end
