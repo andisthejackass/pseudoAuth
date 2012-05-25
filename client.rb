6a5df5226bf682ca9d6bccc157ca0f58ca59662a
Andis Anastasis <andisthejackass@gmail.com>
2012-05-25 17:57:27 +0300

    user can be found in DB

--- client.rb
+++ client.rb
@@ -1,17 +1,15 @@
 require 'socket'
 require 'openssl'
 require 'base64'
+require 'digest'
 
 pub = OpenSSL::PKey::RSA.new(File.read("./pubkey.pem"))
 priv = OpenSSL::PKey::RSA.new(File.read("./privkey.pem"),'1234')
 user = 'andis'
-cipher = Base64.encode64(pub.public_encrypt(user))
-clear = priv.private_decrypt(Base64.decode64(cipher))
+cipher = Base64.encode64(Digest::SHA256.new.digest user)
 
 clientSession = TCPSocket.new( "localhost", 1337 )
-puts "log: starting connection"
-puts "log: saying hello"
-clientSession.puts "Client: Hello Server World!\n"
+clientSession.puts cipher
   while !(clientSession.closed?) && (serverMessage = clientSession.gets)
     puts serverMessage
     if serverMessage.include?("Goodbye")
--- server.rb
+++ server.rb
@@ -4,19 +4,22 @@ require 'active_record'
 require 'yaml'
 require_relative 'active_record_init.rb'
 
-
-puts User.first.username
-
 puts "Starting up server..."
 server = TCPServer.new(1337)
 while (session = server.accept)
   Thread.start do
-    puts "log: Connection from #{session.peeraddr[2]} at #{session.peeraddr[3]}"
-    puts "log: got input from client"
+    #puts "log: Connection from #{session.peeraddr[2]} at #{session.peeraddr[3]}"
+    #Get encrypted username
     input = session.gets
-    puts input
-    session.puts "Server: Welcome #{session.peeraddr[2]}\n"
-    puts "log: sending goodbye"
+    #Search in DB for username
+    user = User.where(:username => input).first
+    if user == nil
+      puts "User could not be found."
+    else
+      puts "user: " + user.username
+      pub = user.pub
+      puts pub
+    end
     session.puts "Server: Goodbye\n"
   end
 end
