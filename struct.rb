01ab40792bd3bb04f0467dab7a3463834993dc75
Andis Anastasis <andisthejackass@gmail.com>
2012-05-27 04:04:30 +0300

    remove forgotten struct.rb file :P

--- struct.rb
+++
@@ -1,9 +0,0 @@
-class User < Struct.new(:username, :pub)
-    def initialize(un, pub)
-        self.username = un
-        self.pub = pub
-    end
-    def inspect
-        self.values
-    end
-end
